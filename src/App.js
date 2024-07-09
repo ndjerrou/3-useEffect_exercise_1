284c1710e47841a3635b92b7d7eba1c8214599e0
ndjerrou <n.djerrou@gmail.com>
2024-07-09 12:11:21 +0100

    Correction exo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+import ImageDisplay from './components/ImageDisplay';
+
 function App() {
-  return <div className='App'>Hello</div>;
+  const URL_UNSPLASH_API = 'https://api.unsplash.com';
+
+  const UNSPLASH_API_KEY =
+    '3d88026776eb1e2c1a99d700ba790ab5d939bb64e9997069f4d95adf46067cfc';
+
+  const [term, setTerm] = useState('');
+  const [img, setImg] = useState('');
+
+  const handleChange = e => {
+    setTerm(e.target.value);
+  };
+
+  // debounce
+  useEffect(() => {
+    if (term.length >= 4) {
+      async function getRandomPic() {
+        try {
+          const response = await axios(URL_UNSPLASH_API + '/search/photos', {
+            params: { query: term, w: '200', h: '200' },
+            headers: {
+              Authorization: `Client-ID ${UNSPLASH_API_KEY}`,
+            },
+          });
+
+          setImg(response.data.results[0].urls.regular);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+
+      getRandomPic();
+    }
+  }, [term]);
+
+  return (
+    <div>
+      <label>
+        Search for a random photo : {'  '}
+        <input value={term} onChange={handleChange} />
+        {img && <ImageDisplay src={img} />}
+      </label>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ImageDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ImageDisplay({ src }) {
+  return (
+    <div>
+      <img src={src} />
+    </div>
+  );
+}
+
+export default ImageDisplay;
